72a600cc05c6794b7326407e1f6b37ec79fcc36b
ajeeshama <45811881+ajeeshama@users.noreply.github.com>
2018-12-14 15:06:55 +0530

    Create alphabet or not.py

---
+++ alphabet or not.py
@@ -0,0 +1,9 @@
+ Print("Enter '0' for exit.");
+ Ch = input("Enter any character: ");
+      if ch == '0':
+    exit();  
+       else:
+    if((ch>='a' and ch<='z') or (ch>='A' and ch<='Z')):
+    	 Print(ch, "is an alphabet.");
+    else:
+    	Print(ch, "is not an alphabet.")
