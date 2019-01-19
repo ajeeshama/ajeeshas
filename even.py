08e04ba9da67fc720c6c7b3b579ecbfe13b21b36
ajeeshama <45811881+ajeeshama@users.noreply.github.com>
2019-01-19 14:42:18 +0530

    Update even.py

--- even.py
+++ even.py
@@ -1,5 +1,5 @@
-  lower_limit = int(input("enter the lower limit : "))
-  upper_limit = int(input("enter the upper limit : "))
+  Lower_limit = Int(Input("enter the lower limit : "))
+  Upper_limit = Int(Input("enter the upper limit : "))
   for i in Range(lower_limit,upper_limit+1):
     if(i%2 == 0):
       print(i)
